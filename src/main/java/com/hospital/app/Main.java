1709abf29c87843233aebecd95d2e891d6dd44a9
ariane05149 <arianeabizera6@gmail.com>
2026-03-12 23:14:54 +0200

    implementation of the Main class first by running the databes setup class to allow the creation of tables

--- src/main/java/com/hospital/app/Main.java
+++ src/main/java/com/hospital/app/Main.java
@@ -1,64 +1,19 @@
 package com.hospital.app;
 
 import com.hospital.dao.*;
-import com.hospital.model.*;
-
+import com.hospital.db.DatabaseSetup;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.sql.Date;
-import java.sql.Timestamp;
 
 
 public class Main {
-
     public static void main(String[] args) {
-
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext("com.hospital");
 
+        DatabaseSetup dbSetup = context.getBean(DatabaseSetup.class);
+
+
         DoctorDAO doctorDAO = context.getBean(DoctorDAO.class);
-        PatientDAO patientDAO = context.getBean(PatientDAO.class);
-        AppointmentDAO appointmentDAO = context.getBean(AppointmentDAO.class);
-        MedicalRecordDAO recordDAO = context.getBean(MedicalRecordDAO.class);
-
-        Doctor doctor = new Doctor(
-                "Arsene",
-                "Hirwa",
-                "pediatry",
-                "078888111",
-                "arsen@hospital.com"
-        );
-
-        doctorDAO.saveDoctor(doctor);
-
-        Patient patient = new Patient(
-                "Ariane",
-                "fanny",
-                Date.valueOf("2005-04-10"),
-                "Female",
-                "078888222",
-                "arie@mail.com"
-        );
-
-        patientDAO.savePatient(patient);
-
-        Appointment appointment = new Appointment(
-                1,
-                1,
-                Timestamp.valueOf("2026-03-20 10:00:00"),
-                "Scheduled"
-        );
-
-        appointmentDAO.saveAppointment(appointment);
-
-        MedicalRecord record = new MedicalRecord(
-                1,
-                1,
-                "Flu",
-                "Medication"
-        );
-
-        recordDAO.saveRecord(record);
 
         context.close();
     }
